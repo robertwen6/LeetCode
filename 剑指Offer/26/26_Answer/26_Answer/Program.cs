2731d82bc7707d4536c59db91c31612564df2816
RobertWen <robertwen16@163.com>
2022-06-16 21:13:05 +0800

    20220616

---
+++ 剑指Offer/26/26_Answer/26_Answer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_Answer
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    public class Solution
+    {
+        public bool IsSubStructure(TreeNode A, TreeNode B)
+        {
+            if (A == null || B == null) return false;
+            return IsAHasB(A, B) || IsSubStructure(A.left, B) || IsSubStructure(A.right, B);
+        }
+        public bool IsAHasB(TreeNode A, TreeNode B)
+        {
+            if (B == null) return true;
+            if (A == null) return false;
+            if (A.val != B.val) return false;
+            return IsAHasB(A.left, B.left) && IsAHasB(A.right, B.right);
+        }
+    }
+}
---
+++ 剑指Offer/27/27_Answer/27_Answer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_Answer
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    
+public class Solution
+    {
+        public TreeNode MirrorTree(TreeNode root)
+        {
+            if (root == null) return null;
+            TreeNode newTree = new TreeNode(0);
+            newTree.val = root.val;
+            newTree.left = MirrorTree(root.right);
+            newTree.right = MirrorTree(root.left);
+            return newTree;
+        }
+    }
+}
