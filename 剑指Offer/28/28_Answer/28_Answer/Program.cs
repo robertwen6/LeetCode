c1475ae45fa8b1919b04f0567cf5e67a5e27591d
RobertWen <robertwen16@163.com>
2022-06-17 22:35:12 +0800

    20220617

---
+++ 剑指Offer/28/28_Answer/28_Answer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Answer
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    
+    public class Solution
+    {
+        List<int> leftArray = new List<int>();
+        List<int> rightArray = new List<int>();
+
+        public bool IsSymmetric(TreeNode root)
+        {
+            MakeLeftArray(root);
+            MakeRightArray(root);
+            for(int i = 0; i < leftArray.Count; i++)
+            {
+                if(leftArray[i] != rightArray[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void MakeLeftArray(TreeNode root)
+        {
+            if (root == null)
+            {
+                leftArray.Add(-1);
+            }
+            else
+            {
+                leftArray.Add(root.val);
+                MakeLeftArray(root.left);
+                MakeLeftArray(root.right);
+            }
+            return;
+        }
+
+        public void MakeRightArray(TreeNode root)
+        {
+            if (root == null)
+            {
+                rightArray.Add(-1);
+            }
+            else
+            {
+                rightArray.Add(root.val);
+                MakeRightArray(root.right);
+                MakeRightArray(root.left);
+            }
+        }
+    }
+}
---
+++ 剑指Offer/29/29_Answer/29_Answer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_Answer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int[] SpiralOrder(int[][] matrix)
+        {
+            if(matrix.Length == 0)
+            {
+                return (new int[0]);
+            }
+            List<int> list = new List<int>();
+            int rowStart = 0;
+            int colStart = 0;
+            int rowEnd = matrix.Length - 1;
+            int colEnd = matrix[0].Length - 1;
+            Console.WriteLine("{0},{1},{2},{3}",rowStart, rowEnd, colStart, colEnd);
+
+            while (rowStart <= rowEnd && colStart <= colEnd)
+            {
+                for(int col = colStart; col <= colEnd; col++)
+                {
+                    list.Add(matrix[rowStart][col]);
+                }
+                for(int row = rowStart + 1; row < rowEnd; row++)
+                {
+                    list.Add(matrix[row][colEnd]);
+                }
+                if (rowStart == rowEnd) break;
+                for (int col = colEnd; col >= colStart; col--)
+                {
+                    list.Add(matrix[rowEnd][col]);
+                }
+                if (colStart == colEnd) break;
+                for (int row = rowEnd - 1; row > rowStart; row--)
+                {
+                    list.Add(matrix[row][colStart]);
+                }
+
+                rowStart++;
+                rowEnd--;
+                colStart++;
+                colEnd--;
+            }
+            return list.ToArray();
+        }
+    }
+}
