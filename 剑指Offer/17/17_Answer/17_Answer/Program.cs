ac5d57248f6f741d7f27b78b9fb11b57b76b99f9
RobertWen <robertwen16@163.com>
2022-06-27 13:16:05 +0800

    20220627 13:15

---
+++ 剑指Offer/17/17_Answer/17_Answer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Answer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int[] PrintNumbers(int n)
+        {
+            int maxNum = GetMaxNum(n);
+            int[] result = new int[maxNum];
+            for(int i = 1; i <= maxNum; i++)
+            {
+                result[i - 1] = i;
+            }
+            return result;
+        }
+
+        public int GetMaxNum(int n)
+        {
+            int maxNum = 0;
+            while(n > 0)
+            {
+                maxNum = maxNum * 10 + 9;
+                n--;
+            }
+            return maxNum;
+        }
+    }
+}
