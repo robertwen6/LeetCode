806df9daa35127346686b747b5b46056471d7ef4
RobertWen <robertwen16@163.com>
2022-07-01 20:03:06 +0800

    20220701 20:03

--- 剑指Offer/32/32_Answer/32_Answer/Program.cs
+++ 剑指Offer/32/32_Answer/32_Answer/Program.cs
@@ -24,27 +24,57 @@ namespace _32_Answer
  */
     public class Solution
     {
-        public int[] LevelOrder(TreeNode root)
+        public IList<IList<int>> LevelOrder(TreeNode root)
         {
-            List<int> l = new List<int>();
+            IList<IList<int>> l = new List<IList<int>>();
             Queue<TreeNode> q = new Queue<TreeNode>();
+            Stack<TreeNode> s = new Stack<TreeNode>();
+
+            int numLayer = 0;
 
             q.Enqueue(root);
             while (q.Count > 0)
             {
-                TreeNode node = q.Dequeue();
-                if (node == null)
+                List<int> lLayer = new List<int>();
+                int lCount = q.Count;
+
+                for (int i = 0; i < lCount; i++)
                 {
-                    continue;
+                    TreeNode node = q.Dequeue();
+                    if (node == null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        lLayer.Add(node.val);
+                        if (numLayer % 2 == 0)
+                        {
+                            s.Push(node.left);
+                            s.Push(node.right);
+                        }
+                        else
+                        {
+                            s.Push(node.right);
+                            s.Push(node.left);
+                        }
+                    }
                 }
-                else
+
+                int sCount = s.Count;
+                for (int i = 0; i < sCount; i++)
                 {
-                    l.Add(node.val);
-                    q.Enqueue(node.left);
-                    q.Enqueue(node.right);
+                    q.Enqueue(s.Pop());
+                }
+
+                numLayer++;
+
+                if (lLayer.Count > 0)
+                {
+                    l.Add(lLayer);
                 }
             }
-            return l.ToArray();
+            return l;
         }
     }
 }
